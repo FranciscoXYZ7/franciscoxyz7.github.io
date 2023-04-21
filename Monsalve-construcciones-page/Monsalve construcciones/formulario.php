5ad00fe38c54c31cf3c8bbd1d12436f34a19658a
Francisco Padilla <126736804+FranciscoXYZ7@users.noreply.github.com>
2023-04-21 13:13:02 -0400

    ssssssssssssssssssss

---
+++ Monsalve-construcciones-page/Monsalve construcciones/formulario.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$lname = $_POST['apellido'];
+$mail = $_POST['email'];
+$tel = $_POST['tel'];
+$comentarios = $_POST['comentarios'];
+
+$para = 'franciscoxyz7@gmail.com';
+$asunto = 'prueba php';
+
+$comentarios = "Este mensaje fue enviado por: $name \n",
+$comentarios .= "Su e-mail es: $mail \n";
+$comentarios .= "Telefono de contacto: $phone \n";
+$comentarios .= "Mensaje: $comentarios";
+
+
+
+mail($para, $asunto, ,$comentarios);
+
+header('Location:exito.html')
+
+?>
