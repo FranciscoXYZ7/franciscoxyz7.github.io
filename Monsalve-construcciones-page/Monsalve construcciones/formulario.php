ac4a1ebefa469b83acef55cca1d0c21828f3ee5b
Francisco <126736804+FranciscoXYZ7@users.noreply.github.com>
2023-04-27 17:08:24 -0400

    Update formulario.php

--- Monsalve-construcciones-page/Monsalve construcciones/formulario.php
+++ Monsalve-construcciones-page/Monsalve construcciones/formulario.php
@@ -8,15 +8,10 @@ $comentarios = $_POST['comentarios'];
 $para = 'franciscoxyz7@gmail.com';
 $asunto = 'prueba php';
 
-$comentarios = "Este mensaje fue enviado por: $name \n",
-$comentarios .= "Su e-mail es: $mail \n";
+$comentarios = "Este mensaje fue enviado por: $name\n";
+$comentarios .= "Su e-mail es: $mail\n";
 $comentarios .= "Telefono de contacto: $phone \n";
 $comentarios .= "Mensaje: $comentarios";
 
-
-
 mail($para, $asunto ,$comentarios);
-
-header('Location:')
-
 ?>
